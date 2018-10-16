0613a6b2d41c344e75de7463e53c6f28f5527bc6
Mona <maharjan1235@gmail.com>
2018-10-16 16:04:29 -0400

    Initial Commit

---
+++ src/TriangleAnalyzer.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author mona
+ */
+public class TriangleAnalyzer 
+{
+    // Instance variables
+    private double sideA; // Lengths of the 3 sides of a triangle
+    private double sideB;
+    private double sideC;   
+    
+    // Create triangle of lengths sideA, sideB and sideC
+    public TriangleAnalyzer(double sideA, double sideB, double sideC)
+    {
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+    }
+    
+    public String toString()
+    {
+        return sideA + "," + sideB + "," + sideC;
+    }
+     
+    // Right angle triangle
+    public boolean Right()
+    {
+        if ((sideA * sideA) == ((sideB * sideB) + (sideC * sideC)))
+        {
+            return true;
+        }
+        else if ((sideB * sideB) == ((sideA * sideA) + (sideC * sideC)))
+        {
+            return true;
+        }
+        else if (((sideC * sideC) == ((sideB * sideB) + (sideA * sideA))))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    //Obtuse Triangle
+    public boolean Obtuse()
+    {
+        if(((sideA + sideB) *(sideA + sideB)) <= (sideC * sideC))
+        {
+            return true;
+        }
+        else if(((sideB + sideC) *(sideB + sideC)) <= (sideA * sideA))
+        {
+            return true;
+        }
+        if(((sideC + sideA) + (sideC + sideA)) <= (sideB * sideB)) 
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
+    }
+    // Get the area of a triangle
+    public double getArea()
+    {
+        /**Area using the Heron's formula
+         * s = half perimeter
+         * area = the squareroot of (s(s -a) (s - b) (s-c))
+         * where, a, b and c are the length of the sides
+         */
+        double halfPerim = (sideA + sideB + sideC) / 2;
+        double area = Math.sqrt(halfPerim * ( halfPerim - sideA) * (halfPerim - sideB)
+                        * (halfPerim - sideC));
+        return area;
+    }
+    // Type of a triangle
+    public String triangleType()
+    {
+        if (sideA == sideB  && sideA == sideC)
+        {
+            return ("\n\tEquilateral Triangle");
+        }
+        else if (sideA == sideB || sideA == sideC || sideB == sideC)
+        {
+            return("\n\tIsoscleles Triangle");
+        }
+        else if (Right() == true)
+        {
+            return ("\n\tRight Angle Triangle");
+        }
+        else if (Obtuse() == true)
+        {
+            return ("\n\tObtuse Triangle");
+        }
+        else if (Right() == false && Obtuse() == false)
+        {
+            return ("\n\tAcute Triangle");
+        }
+        else
+        {
+            return ("\n\tNot a triangle");
+        }
+            
+        }
+    
+}
+
---
+++ src/TriangleAnalyzerTest.java
@@ -0,0 +1,42 @@
+
+import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mona
+ */
+public class TriangleAnalyzerTest 
+{
+    public static void main(String args[])
+    {
+        // Get input from the user
+        String input = JOptionPane.showInputDialog(
+                "Enter side one for sideA");
+
+        double sideA  = Double.parseDouble(input);   // take input as double
+        
+        input = JOptionPane.showInputDialog(
+                    "Enter side two for sideB");
+        double sideB = Double.parseDouble(input);
+        
+        input = JOptionPane.showInputDialog(
+                "Enter side three for sideC");
+        double sideC = Double.parseDouble(input);
+        
+        //Create Triangle Analyzer object
+        TriangleAnalyzer triangle = new TriangleAnalyzer(sideA, sideB, sideC);
+        System.out.println("A triangle with sides of " + triangle.toString() 
+                        + "\nThe triangle is of type " + triangle.triangleType()
+                        + "\nArea of the triangle is " + 
+                        String.format("%7.2f", triangle.getArea());
+        
+        
+    }
+    
+}
